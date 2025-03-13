82242b49ba7b4a512f6d6a021cd42d55e922c9af
MatthewSperling <148011258+MatthewSperling@users.noreply.github.com>
2025-03-12 19:03:02 -0600

    part b

--- app.js
+++ app.js
@@ -14,8 +14,8 @@ mongoose.connect(process.env.DB_CONNECTION || 'mongodb://127.0.0.1:27017/authent
   useNewUrlParser: true,
   useUnifiedTopology: true
 })
-.then(() => console.log('MongoDB connected'))
-.catch(err => console.error('MongoDB connection error:', err));
+  .then(() => console.log('MongoDB connected'))
+  .catch(err => console.error('MongoDB connection error:', err));
 
 // Middleware
 app.use(helmet());
